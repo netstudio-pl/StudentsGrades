9635a27c21400186dc38001b91f297e13f1f5ed1
Rafał Fijałkowski <pw06177@pwpw.pl>
2018-06-06 15:15:13 +0200

    Added StudentsGrades project filess

---
+++ StudentsGrades/Models/Grade.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentsGrades.Models
+{
+    public class Grade
+    {
+        public decimal Value { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ StudentsGrades/Services/StudentGradesService.cs
@@ -0,0 +1,20 @@
+﻿using StudentsGrades.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentsGrades.Services
+{
+    public class StudentGradesService
+    {
+        /// <summary>
+        /// Metoda obliczająca średnią ważoną przekazanej kolekcji ocen studentów
+        /// </summary>
+        /// <returns></returns>
+        public decimal Calculate(IEnumerable<Grade> grades)
+        {
+            //obliczam średnią ważoną ocen (korzystam z LINQ i lambda)
+            var gradesSum = grades.Sum(g => g.Value * g.Weight); //suma iloczynów ocen i ich wag
+            return gradesSum / grades.Sum(g => g.Weight); //podzielona przes sumę wag ocen
+        }
+    }
+}
